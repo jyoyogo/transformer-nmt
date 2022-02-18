6a5eef2fd4dab5f4600fb45d0320054b458f089f
KJBigdata <kj.bigdata@gmail.com>
2022-02-18 14:23:00 +0900

    add apply nmt function and data

--- translate.sh
+++ translate.sh
@@ -1,8 +1,8 @@
 echo "$(printf "**ref(test) source**")"
-head -n 5 data/corpus.sample.test.tok.bpe.en | python ../nmt-preprocess/detokenizer.py
+head -n 5 data/corpus.sample.test.tok.bpe.en | python apply_nmt/detokenizer.py
 echo "$(printf "\n\n")"
 echo "$(printf "**ref(test) target**")"
-head -n 5 data/corpus.sample.test.tok.bpe.ko | python ../nmt-preprocess/detokenizer.py
+head -n 5 data/corpus.sample.test.tok.bpe.ko | python apply_nmt/detokenizer.py
 echo "$(printf "\n\n")"
 echo "$(printf "**inference result, beam_size 5**")"
-head -n 5 data/corpus.sample.test.tok.bpe.en | python translate.py --model_fn model/nmt_model.30.*.pth --gpu_id -1 --batch_size 2 --beam_size 1 | python ../nmt-preprocess/detokenizer.py
+head -n 5 data/corpus.sample.test.tok.bpe.en | python translate.py --model_fn checkpoint/nmt_model.30.*.pth --gpu_id -1 --batch_size 2 --beam_size 1 | python apply_nmt/detokenizer.py
