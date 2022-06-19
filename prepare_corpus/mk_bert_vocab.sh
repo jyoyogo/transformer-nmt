f78c7f73173177835477cf852574760ebeee2c02
jyoyogo <kj.bigdata@gmail.com>
2022-06-19 22:15:35 +0900

    rm bert wordpiece module&sh

--- prepare_corpus/mk_bert_vocab.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python3 train_bertwordpiece.py --type 'src' --files 'pretokenized_corpus/*.train.tok.ko' --name 'ko.bert-wordpiece' --out 'vocab'
-python3 train_bertwordpiece.py --type 'tgt' --files 'pretokenized_corpus/*.train.tok.en' --name 'en.bert-wordpiece' --out 'vocab'
